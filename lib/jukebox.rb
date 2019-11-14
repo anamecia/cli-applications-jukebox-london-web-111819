a60c5d18326189fbeefd6a643d4dbdc843a71442
Ana Robalo <anameciarobalo@gmail.com>
2019-11-14 18:43:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,11 +27,11 @@ end
 def play (songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
-  list_songs = list(songs)
+  #list_songs = list(songs)
   i = 0
   while i < list_songs.length do 
-    if list_songs[i].include? user_input
-      puts "Playing #{list_songs[i]}"
+    if songs[i].include? user_input
+      puts "Playing #{songs[i]}"
       return
     end
     i += 1
